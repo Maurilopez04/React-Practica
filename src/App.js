bcb04eeeeefd46f07e285a52066920be5053999d
Mauricio Lopez <maurilopez04@gmail.com>
2024-01-23 23:43:59 -0300

    Commit 3 react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+// App.js
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import Home from './home'
+import Tienda from './Tienda'
+import AboutUs from './AboutUs'
+import Servicios from './Servicios'
+import Blog from './Blog'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/tienda" component={Tienda} />
+                <Route path="/aboutus" component={AboutUs} />
+                <Route path="/servicios" component={Servicios} />
+                <Route path="/blog" component={Blog} />
+            </Switch>
+        </Router>
+    )
 }
 
-export default App;
+export default App
---
+++ src/components/headerr.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Headerr = () => {
+    return (
+        <div>
+            <h1>Holaaaa</h1>
+            <h1>Holaaaa</h1>
+            <h1>Holaaaa</h1>
+            <h1>Holaaaa</h1>
+        </div>
+    )
+}
+export default Headerr
